c89414b290b21805c781563a07b89d132c6f4df4
Pekka Mansikka <pekka2@users.noreply.github.com>
2023-09-14 09:38:35 +0300

    Create failure.php

---
+++ Opencart_1.5.6/catalog/language/finnish/checkout/failure.php
@@ -0,0 +1,11 @@
+<?php
+// Heading 
+$_['heading_title'] = 'Virhe';
+
+// Text
+$_['text_basket']   = 'Ostoskori';
+$_['text_shipping'] = 'Toimitus';
+$_['text_payment']  = 'Maksu';
+$_['text_failure']  = 'Virhe';
+$_['text_message']  = '<p>Tilauksen käsittelyssä ilmeni virhe!</p><p>Jos ongelma ei poistu, valitse toinen maksutapa tai ole yhteydessä verkkokaupan omistajaan <a href="%s">klikkaamalla tässä</a>.';
+?>

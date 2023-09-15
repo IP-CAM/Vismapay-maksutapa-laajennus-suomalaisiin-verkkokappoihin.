0536cbc9f22adc614587dcb401b37d61fb461417
Pekka Mansikka <pekka2@users.noreply.github.com>
2023-09-15 11:47:15 +0300

    Create vismapay.php

---
+++ Opencart_2.0-2.1/admin/language/finnish/vismapay.php
@@ -0,0 +1,24 @@
+<?php
+// Heading
+$_['heading_title']        = 'Vismapay';
+
+// Text
+$_['text_edit']           = 'Muokkaa';
+$_['text_clear']          = 'Tyhjennä Vismapay loki';
+$_['text_clear_success']  = 'Lokitietojen tyhjennys onnistui!';
+
+//entry
+$_['entry_api_key']        = 'API tunnisteavain';
+$_['entry_private_key']    = 'Yksityinen salausavain';
+$_['entry_order_status']   = 'Tilauksen tila';
+$_['entry_failed_status']  = 'Epäonnistuneen maksun tila';
+$_['entry_geo_zone']       = 'Maantiet. alue';
+$_['entry_status']         = 'Tila';
+$_['entry_sort_order']     = 'Järjestys:';
+$_['entry_log']            = 'Loki';
+
+// Error
+$_['error_permission']      = 'Varoitus: Sinulla ei ole oikeuksia Vismapay maksutavan muokkaamiseen!';
+$_['error_api_key']         = 'API tunniste on pakollinen tieto!';
+$_['error_private_key']     = 'Salausavain on pakollinen tieto!';
+?>

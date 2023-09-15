3c7a395366c982736dcef420a86b62b6c91f676c
Pekka Mansikka <pekka2@users.noreply.github.com>
2023-09-15 12:00:17 +0300

    Create vismapay.php

---
+++ Opencart_2.0-2.1/system/library/vismapay.php
@@ -0,0 +1,6 @@
+<?php
+
+require_once dirname(__FILE__) . '/Vismapay/PayForm.php';
+require_once dirname(__FILE__) . '/Vismapay/PayFormConnector.php';
+require_once dirname(__FILE__) . '/Vismapay/PayFormCurl.php';
+require_once dirname(__FILE__) . '/Vismapay/PayFormException.php';
